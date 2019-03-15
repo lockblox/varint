355799bea5d5f972479284589a66151e6a613844
jbrooker <jbrooker@users.noreply.github.com>
2019-03-15 19:34:00 +0000

    Implemented varint value type and associated tests

---
+++ test/uleb128_test.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+#include <varint/codecs/uleb128.h>
+#include <array>
+
+class encoding_test
+    : public testing::TestWithParam<std::pair<std::size_t, std::string>> {};
+
+class round_trip_test : public testing::TestWithParam<std::size_t> {};
+
+TEST_P(encoding_test, encoding) {
+  auto [input, expected] = GetParam();
+  auto result = std::string{};
+  auto size = varint::codecs::uleb128::encode(input, std::back_inserter(result));
+  EXPECT_EQ(expected.size(), size);
+  EXPECT_TRUE(std::equal(result.begin(), result.end(), expected.begin()));
+  EXPECT_EQ(result.size(), varint::codecs::uleb128::size(input));
+  EXPECT_EQ(result, result);
+}
+
+TEST_P(round_trip_test, round_trip) {
+  using uleb128 = varint::codecs::uleb128;
+  auto input = GetParam();
+  auto encoded = std::string{};
+  auto size = varint::codecs::uleb128::encode(input, std::back_inserter(encoded));
+  auto decoded = varint::codecs::uleb128::decode(encoded.begin(), encoded.end());
+  EXPECT_EQ(input, decoded);
+}
+
+INSTANTIATE_TEST_CASE_P(
+    varint, encoding_test,
+    ::testing::Values(std::pair{0, std::string{0}},
+                      std::pair{127, std::string{127}},
+                      std::pair{128, std::string{-128, 1}},
+                      std::pair{255, std::string{-1, 1}},
+                      std::pair{300, std::string{-84, 2}},
+                      std::pair{16384, std::string{-128, -128, 1}}));
+
+INSTANTIATE_TEST_CASE_P(varint, round_trip_test,
+                        ::testing::Values(0, 127, 128, 255, 256, 32767, 32768,
+                                          65535, 65536, 2147483647, 2147483648,
+                                          4294967295, 4294967296,
+                                          9223372036854775807,
+                                          9223372036854775808,
+                                          18446744073709551615));
--- test/varint_test.cpp
+++ test/varint_test.cpp
@@ -1,39 +1,40 @@
 #include <gtest/gtest.h>
-#include <varint/uleb128.h>
+#include <varint/codecs/uleb128.h>
 #include <varint/varint.h>
+#include <array>
 
-class encoding_test
-    : public testing::TestWithParam<std::pair<std::size_t, std::string>> {};
-
-class round_trip_test : public testing::TestWithParam<std::size_t> {};
-
-TEST_P(encoding_test, encoding) {
-  auto [input, expected] = GetParam();
-  auto uleb128 = varint::uleb128();
-  auto result = uleb128.encode(input);
-  EXPECT_EQ(expected, result);
-  EXPECT_EQ(result.size(), uleb128.encoded_size(input));
+TEST(varint, compare) {
+  using array = std::array<char, 2>;
+  using varint_array = varint::uleb128<array>;
+  using varint_string = varint::uleb128<std::string>;
+  auto via = varint_array{128u};
+  auto vis = varint_string{{-128, 1}};
+  EXPECT_EQ(via, vis);
 }
 
-TEST_P(round_trip_test, round_trip) {
-  auto input = GetParam();
-  auto uleb128 = varint::uleb128();
-  auto encoded = uleb128.encode(input);
-  EXPECT_EQ(input, uleb128.decode(encoded));
+TEST(varint, assignment) {
+  using array = std::array<char, 2>;
+  using varint_array = varint::uleb128<array>;
+  using inserter = std::back_insert_iterator<array>;
+  varint_array via;
+  via = 1233u;
+  auto copy = via;
+  EXPECT_EQ(via, copy);
+  EXPECT_THROW(via = 65536u, std::out_of_range);
+  EXPECT_THROW(static_cast<char>(via), std::overflow_error);
+  EXPECT_EQ(static_cast<unsigned int>(via), 1233);
 }
 
-INSTANTIATE_TEST_CASE_P(
-    varint, encoding_test,
-    ::testing::Values(std::pair{0, std::string{0}},
-                      std::pair{127, std::string{127}},
-                      std::pair{128, std::string{-128, 1}},
-                      std::pair{255, std::string{-1, 1}},
-                      std::pair{300, std::string{-84, 2}},
-                      std::pair{16384, std::string{-128, -128, 1}}));
-
-INSTANTIATE_TEST_CASE_P(
-    varint, round_trip_test,
-    ::testing::Values(0, 127, 128, 255, 256, 32767, 32768, 65535, 65536,
-                      2147483647, 2147483648, 4294967295, 4294967296,
-                      9223372036854775807, 9223372036854775808,
-                      18446744073709551615));
+TEST(varint, input_stream) {
+  std::string input_string = {-128, 1, 'h', 'e', 'l', 'l', 'o'};
+  std::istringstream is{input_string};
+  using varint_string = varint::uleb128<std::string>;
+  varint_string vi;
+  is >> vi;
+  EXPECT_EQ(vi, static_cast<varint_string>(128u));
+  auto hello = std::string{};
+  std::copy(std::istreambuf_iterator<char>(is),
+              std::istreambuf_iterator<char>(),
+              std::back_inserter(hello));
+  EXPECT_EQ("hello", hello);
+}
