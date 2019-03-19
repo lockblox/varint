fd6dc6ffe4efd095dc482ca5e533b0246ddafa93
jbrooker <jbrooker@users.noreply.github.com>
2019-03-19 07:49:39 +0000

    Added test case for empty input in uleb128

--- test/uleb128_test.cpp
+++ test/uleb128_test.cpp
@@ -25,6 +25,8 @@ TEST_P(round_trip_test, round_trip) {
   uleb128::encode(input, std::back_inserter(encoded));
   auto decoded = uleb128::decode(encoded.begin(), encoded.end());
   EXPECT_EQ(input, decoded);
+  EXPECT_THROW(auto output = uleb128::decode(encoded.end(), encoded.end()),
+               std::out_of_range);
 }
 
 INSTANTIATE_TEST_CASE_P(
